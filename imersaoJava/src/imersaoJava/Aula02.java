065481ab792056bb9955252a83c3ce6c0f093936
GabsSantos <gabriele.ss@icloud.com>
2023-03-28 21:21:57 -0300

    Stickers - Aula 02

--- imersaoJava/src/imersaoJava/Aula02.java
+++ imersaoJava/src/imersaoJava/Aula02.java
@@ -1,42 +1,78 @@
 package imersaoJava;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Font;
+import java.awt.FontMetrics;
 import java.awt.Graphics2D;
+import java.awt.Shape;
+import java.awt.font.FontRenderContext;
+import java.awt.font.TextLayout;
+import java.awt.geom.AffineTransform;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.InputStream;
 
 import javax.imageio.ImageIO;
 
 public class Aula02 {
     
-    public void gerandoFigurinhas() throws Exception {
 
-        BufferedImage imagemOriginal = ImageIO.read(new File("entrada/PulpFiction.jpg"));
+    public void cria(InputStream inputStream, String nomeArquivo, String texto, InputStream inputStreamSobreposicao) throws Exception {
 
+        // leitura da imagem
+        // InputStream inputStream = 
+        //             new FileInputStream(new File("entrada/filme-maior.jpg"));
+        // InputStream inputStream = 
+        //                 new URL("https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@.jpg")
+        //                 .openStream();
+        BufferedImage imagemOriginal = ImageIO.read(inputStream);
+
+        // cria nova imagem em memória com transparência e com tamanho novo
         int largura = imagemOriginal.getWidth();
         int altura = imagemOriginal.getHeight();
-        int novaAltura = altura + 200;
+        int novaAltura = altura + 290;
         BufferedImage novaImagem = new BufferedImage(largura, novaAltura, BufferedImage.TRANSLUCENT);
 
+        // copiar a imagem original pra novo imagem (em memória)
         Graphics2D graphics = (Graphics2D) novaImagem.getGraphics();
         graphics.drawImage(imagemOriginal, 0, 0, null);
 
-        var fonte = new Font(Font.SANS_SERIF, Font.BOLD, 64);
-        graphics.setColor(Color.BLACK);
+        BufferedImage imagemSobreposicao = ImageIO.read(inputStreamSobreposicao);
+        graphics.drawImage(imagemSobreposicao, 0, 1400, null);
+        
+        // configurar a fonte
+        var fonte = new Font(Font.MONOSPACED, Font.ITALIC, 100);
+        graphics.setColor(Color.YELLOW);
         graphics.setFont(fonte);
 
-        graphics.drawString("O melhor filme!", 100, novaAltura - 100);
+        // escrever uma frase na nova imagem
+        FontMetrics fontMetrics = graphics.getFontMetrics();
+        Rectangle2D retangulo = fontMetrics.getStringBounds(texto, graphics);
+        int larguraTexto = (int)retangulo.getWidth();
+        int posicaoTextoX = (largura - larguraTexto) / 2;
+        int posicaoTextoY = novaAltura - 100;
+        graphics.drawString(texto, posicaoTextoX, posicaoTextoY);
+        
+        FontRenderContext fontRenderContext = graphics.getFontRenderContext();
+        var textLayout = new TextLayout(texto , fonte, fontRenderContext);
+        
+        Shape outline = textLayout.getOutline(null);
+        AffineTransform transform = graphics.getTransform();
+        transform.translate(posicaoTextoX, posicaoTextoY);
+        graphics.setTransform(transform);
+        
+        var outlineStroke = new BasicStroke(largura * 0.004f);
+        graphics.setStroke(outlineStroke);
+        
+        graphics.setColor(Color.BLACK);
+        graphics.draw(outline);
+        graphics.setClip(outline);
 
-        ImageIO.write(novaImagem, "png", new File("saida/figurinha.png"));
+        // escrever a nova imagem em um arquivo
+        ImageIO.write(novaImagem, "png", new File(nomeArquivo));
 
     }
-    
-    
-    public static void main(String[] args) throws Exception {
-		var geradora = new Aula02();
-		geradora.gerandoFigurinhas();
-		
-	}
 
 }
